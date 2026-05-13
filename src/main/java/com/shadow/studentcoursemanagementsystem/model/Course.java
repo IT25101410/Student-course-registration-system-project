3145f48e4d083e315071a071eb8c53d930f87b67
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-13 09:10:37 +0530

    Create annotated Course entity

--- src/main/java/com/shadow/studentcoursemanagementsystem/model/Course.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/model/Course.java
@@ -1,4 +1,18 @@
 package com.shadow.studentcoursemanagementsystem.model;
 
-public class Course {
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="courses")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE )
+@DiscriminatorColumn(name = "course_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public abstract class Course {
+
+
 }
