1c44668e68f082e9ca0ffb055d95059559354cc0
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-13 17:10:48 +0530

    Variable map to the column of database

--- src/main/java/com/shadow/studentcoursemanagementsystem/model/Course.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/model/Course.java
@@ -18,9 +18,14 @@ public abstract class Course {
 
     //Attributes
     private Long id;
+    // variable map to the column of database
+    @Column(nullable = false, unique = true) //unique = true (cant value duplicate)
     private String courseCode;
+    @Column(nullable = false)// nullable = false(cant this  column empty )
     private String courseName;
+    @Column(nullable = false)
     private String department;
+    @Column(nullable = false)
     private int credits;
 
 
