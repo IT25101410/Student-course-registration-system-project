65c8912c7abbc677e35f10628f1e344537c1ee47
Thamuditha Kewan <IT25101098@my.sliit.lk>
2026-05-15 15:45:36 +0530

    Test 3

--- src/main/java/com/sliit/scrs/model/PostgraduateStudent.java
+++ src/main/java/com/sliit/scrs/model/PostgraduateStudent.java
@@ -4,8 +4,8 @@ import jakarta.persistence.DiscriminatorValue;  //
 import jakarta.persistence.Entity;  //
 import lombok.AllArgsConstructor;  //
 import lombok.Data;   //
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;  //
+import lombok.NoArgsConstructor;  //
 
 @Entity
 @DiscriminatorValue("POSTGRADUATE")
