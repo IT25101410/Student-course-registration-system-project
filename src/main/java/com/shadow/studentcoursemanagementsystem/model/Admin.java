0e624fdeeac8d9711700a9359a524bd995c0e55a
IT25100499 <IT25100499@my.sliit.lk>
2026-05-16 01:42:49 +0530

    Admin entity model

---
+++ src/main/java/com/shadow/studentcoursemanagementsystem/model/Admin.java
@@ -0,0 +1,36 @@
+package com.shadow.studentcoursemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+    @Entity
+    @Table(name = "admins")
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Admin {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false, unique = true)
+        private String username;
+
+        // Encapsulation: password stored securely
+        @Column(nullable = false)
+        private String password;
+
+        @Column(nullable = false)
+        private String fullName;
+
+        @Column(nullable = false)
+        private String email;
+
+        @Column(nullable = false)
+        private String role; // "SUPER_ADMIN" or "ADMIN"
+    }
+
+
+
