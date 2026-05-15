a9df8e6eb122a4ed7f12191bb5b37a7f482c953a
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-15 17:50:19 +0530

    Create the anotation in  ElectiveCourse class

--- src/main/java/com/shadow/studentcoursemanagementsystem/model/ElectiveCourse.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/model/ElectiveCourse.java
@@ -1,5 +1,16 @@
 package com.shadow.studentcoursemanagementsystem.model;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("ELECTIVE")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class ElectiveCourse extends Course{
 
 }
