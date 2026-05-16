b6e3a9b9b26cefe789ceff2c3ffc2f82c8a891ad
it25101857 <it25101857@my.sliit.lk>
2026-05-16 18:10:07 +0530

    feat: Add EnrollmentController with REST endpoints for enrollment management

--- src/main/java/com/shadow/studentcoursemanagementsystem/controller/EnrollmentController.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/controller/EnrollmentController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.shadow.studentcoursemanagementsystem.dto.EnrollmentRequestDTO;
+import com.shadow.studentcoursemanagementsystem.service.EnrollmentService;
 import java.util.List;
 
 @RestController
