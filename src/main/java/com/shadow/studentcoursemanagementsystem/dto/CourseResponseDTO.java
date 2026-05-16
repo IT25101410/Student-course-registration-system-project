759d6108eb81d9bf5ced6cde4c255e332a348143
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-16 17:05:03 +0530

    add the variables to CourseResponseDTO clas

--- src/main/java/com/shadow/studentcoursemanagementsystem/dto/CourseResponseDTO.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/dto/CourseResponseDTO.java
@@ -2,8 +2,20 @@ package com.shadow.studentcoursemanagementsystem.dto;
 
 import lombok.*;
 
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseResponseDTO {
+    //Variable
+    private Long id;
+    private String courseCode;
+    private String courseName;
+    private String department;
+    private int credits;
+    private String type;
+    private String description;
+
+
 }
