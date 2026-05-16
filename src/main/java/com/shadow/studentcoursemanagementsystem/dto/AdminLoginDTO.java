6037f72d0e8001e45cd89a4efcd0043ca448f33e
IT25100499 <IT25100499@my.sliit.lk>
2026-05-16 16:47:11 +0530

    Add  for login input

---
+++ src/main/java/com/shadow/studentcoursemanagementsystem/dto/AdminLoginDTO.java
@@ -0,0 +1,11 @@
+package com.shadow.studentcoursemanagementsystem.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminLoginDTO {
+    private String username;
+    private String password;
+}
