906f64e3180e15b245d1296f64c3a1d2f5f17773
it25101857 <it25101857@my.sliit.lk>
2026-05-16 16:03:28 +0530

    feat: Add EnrollmentController with /api/enrollments base REST endpoint mapping

---
+++ src/main/java/com/shadow/studentcoursemanagementsystem/controller/EnrollmentController.java
@@ -0,0 +1,11 @@
+package com.shadow.studentcoursemanagementsystem.controller;
+
+import com.sliit.scrs.dto.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+}
