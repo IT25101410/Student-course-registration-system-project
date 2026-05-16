4ef5e1f40fbf68660978ab578f6f3cf6c3ba491d
it25101857 <it25101857@my.sliit.lk>
2026-05-16 12:38:13 +0530

    feat: Add EnrollmentRepository extending JpaRepository for Enrollment data access

---
+++ src/main/java/com/sliit/scrs/repository/EnrollmentRepository.java
@@ -0,0 +1,7 @@
+package com.sliit.scrs.repository;
+import com.sliit.scrs.model.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
