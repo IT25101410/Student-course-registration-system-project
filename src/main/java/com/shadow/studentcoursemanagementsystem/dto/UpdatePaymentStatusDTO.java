7a57fd19265f270eb0ccbb16627b52fdcd450f43
IT25100499 <IT25100499@my.sliit.lk>
2026-05-17 18:45:55 +0530

    payment status update

---
+++ src/main/java/com/shadow/studentcoursemanagementsystem/dto/UpdatePaymentStatusDTO.java
@@ -0,0 +1,10 @@
+package com.shadow.studentcoursemanagementsystem.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePaymentStatusDTO {
+    private String status; // "PENDING", "COMPLETED", "FAILED"
+}
