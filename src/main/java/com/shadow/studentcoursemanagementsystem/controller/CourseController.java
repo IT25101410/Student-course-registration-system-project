670836600f536de0eecf4d9ce91d30d5e5a2b82b
IT25100499 <IT25100499@my.sliit.lk>
2026-05-17 21:50:18 +0530

    Service and impl

--- src/main/java/com/shadow/studentcoursemanagementsystem/controller/CourseController.java
+++
@@ -1,4 +0,0 @@
-package com.shadow.studentcoursemanagementsystem.controller;
-
-public class CourseController {
-}
--- src/main/java/com/shadow/studentcoursemanagementsystem/service/AdminService.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/service/AdminService.java
@@ -1,4 +1,17 @@
 package com.shadow.studentcoursemanagementsystem.service;
 
-public class AdminService {
+import com.shadow.studentcoursemanagementsystem.dto.AdminLoginDTO;
+import com.shadow.studentcoursemanagementsystem.dto.AdminRegisterDTO;
+import com.shadow.studentcoursemanagementsystem.dto.AdminResponseDTO;
+
+import java.util.List;
+
+public interface AdminService {
+    AdminResponseDTO registerAdmin(AdminRegisterDTO dto);
+    AdminResponseDTO login(AdminLoginDTO dto);
+    List<AdminResponseDTO> getAllAdmins();
+    AdminResponseDTO getAdminById(Long id);
+    void deleteAdmin(Long id);
+    //editttttttttt
+   // SystemStatsDTO getSystemStats();
 }
