b612a768740cc9c61b6e91b1ca11cf4dd0328881
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-18 04:07:05 +0530

    Postmapping in  CourseController

--- src/main/java/com/shadow/studentcoursemanagementsystem/controller/CourseController.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/controller/CourseController.java
@@ -1,11 +1,30 @@
 package com.shadow.studentcoursemanagementsystem.controller;
 
 
+import com.shadow.studentcoursemanagementsystem.dto.CourseRequestDTO;
+import com.shadow.studentcoursemanagementsystem.dto.CourseResponseDTO;
+import com.shadow.studentcoursemanagementsystem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/courses")
+
+@RestController // get HTTP Request from Frontend
+@RequestMapping("/api/courses") // Create public URL path
 public class CourseController {
+
+    //Connect Course Service
+    @Autowired
+    private CourseService courseService;
+
+    // Create the new course
+    @PostMapping
+    public ResponseEntity<CourseResponseDTO>addCourse(@RequestBody CourseRequestDTO dto){
+
+        return ResponseEntity.ok(courseService.addCourse(dto));
+    }
 
 }
