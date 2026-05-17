67fc012ffea4c802633aabd49fad4ab7a096a45a
Thamuditha Kewan <IT25101098@my.sliit.lk>
2026-05-17 22:55:52 +0530

    Add StudentRequestDTO

--- src/main/java/com/shadow/studentcoursemanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/repository/StudentRepository.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 @Repository
 
 public interface StudentRepository extends JpaRepository<Student,Long>{
-    Optional<Student> findByEmail(String email);
-    boolean existsByEmail(String email);
+    Optional<Student> findByEmail(String email);  //fetche student from database using their email
+    boolean existsByEmail(String email);   //this checkes if email already in the DB
 }
 
 
