2d12c9aaed61e490e65d25d35cda819eff10f184
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-18 03:48:18 +0530

    Create public URL path

--- src/main/java/com/shadow/studentcoursemanagementsystem/controller/CourseController.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/controller/CourseController.java
@@ -1,4 +1,11 @@
 package com.shadow.studentcoursemanagementsystem.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+
 }
