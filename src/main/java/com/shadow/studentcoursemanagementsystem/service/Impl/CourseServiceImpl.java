1ec52f8e882b962152c31e7778ceabd12a40dc86
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-17 16:03:24 +0530

    CourseServiceImpl implements CourseService

--- src/main/java/com/shadow/studentcoursemanagementsystem/service/Impl/CourseServiceImpl.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/service/Impl/CourseServiceImpl.java
@@ -1,9 +1,10 @@
 package com.shadow.studentcoursemanagementsystem.service.Impl;
 
 
+import com.shadow.studentcoursemanagementsystem.service.CourseService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CourseServiceImpl {
+public class CourseServiceImpl implements CourseService {
 
 }
