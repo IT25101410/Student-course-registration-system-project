13b60cc137949a648178e5c32ee82e950333f1cf
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-17 16:55:15 +0530

    Connect the Course Repository to Course Service Impl

--- src/main/java/com/shadow/studentcoursemanagementsystem/service/Impl/CourseServiceImpl.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/service/Impl/CourseServiceImpl.java
@@ -1,10 +1,18 @@
 package com.shadow.studentcoursemanagementsystem.service.Impl;
 
 
+import com.shadow.studentcoursemanagementsystem.repository.CourseRepository;
 import com.shadow.studentcoursemanagementsystem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+    //Connect to Course Repository
+    @Autowired
+    private CourseRepository courseRepository;
+
+
 
 }
