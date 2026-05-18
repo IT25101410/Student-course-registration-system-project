73f85a7ae215cca83f41b2eb8402bd2466cd7bdf
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-18 09:59:32 +0530

    Add getAllCourses method to CourseService

--- src/main/java/com/shadow/studentcoursemanagementsystem/service/CourseService.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/service/CourseService.java
@@ -3,7 +3,11 @@ package com.shadow.studentcoursemanagementsystem.service;
 import com.shadow.studentcoursemanagementsystem.dto.CourseRequestDTO;
 import com.shadow.studentcoursemanagementsystem.dto.CourseResponseDTO;
 
+import java.util.List;
+
 //Data input and output
 public interface CourseService {
     CourseResponseDTO addCourse(CourseRequestDTO dto);
+    // Read all Course
+    List<CourseResponseDTO> getAllCourses();
 }
