c0cf33f85dd4cd768d5c5b6242b6a4d692b93284
oshadakaveesh2022 <it25101410@my.sliit.lk>
2026-05-18 10:16:48 +0530

    update the course Repository

--- src/main/java/com/shadow/studentcoursemanagementsystem/repository/CourseRepository.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/repository/CourseRepository.java
@@ -1,15 +1,18 @@
 package com.shadow.studentcoursemanagementsystem.repository;
 
+import com.shadow.studentcoursemanagementsystem.dto.CourseResponseDTO;
 import com.shadow.studentcoursemanagementsystem.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CourseRepository  extends JpaRepository<Course,Long> {
     Optional<Course>findCourseCode(String courseCode);
     boolean existsByCourseCode(String  courseCode);
+
 
 
 }
