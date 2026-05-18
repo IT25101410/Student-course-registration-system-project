ce81b2f6de8f3cfe8e5939724342b70b43e60497
Thamuditha Kewan <IT25101098@my.sliit.lk>
2026-05-18 09:09:22 +0530

    Add some comments

--- src/main/java/com/shadow/studentcoursemanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/shadow/studentcoursemanagementsystem/repository/StudentRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 
-public interface StudentRepository extends JpaRepository<Student,Long>{
+public interface StudentRepository extends JpaRepository<Student,Long>{  //This extend helps to get instant methods - save(Student) findById(ID), deleteById(ID)....
     Optional<Student> findByEmail(String email);  //fetche student from database using their email
     boolean existsByEmail(String email);   //this checkes if email already in the DB
 }
